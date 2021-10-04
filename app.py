b52e0ab0c60d92851dab5eae8b3f7486d3c1ee33
Igor <adasdad@gmail.com>
2021-10-04 12:01:36 +0300

    Upd 1. +Translate from russian to english

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def trans():
     if request.method == 'POST':
-        translator = Translator(to_lang='ru')
-        result = request.form['text_for_trans']
-        result = translator.translate(result)
-        return render_template("index.html", text_after_trans=result)
+        translator_ru = Translator(to_lang='ru')
+        resultRu = request.form['text_for_transToRu']
+        resultRu = translator_ru.translate(resultRu)
+        translator_en = Translator(to_lang='en', from_lang='ru')
+        resultEn = request.form['text_for_transToEn']
+        resultEn = translator_en.translate(resultEn)
+        return render_template("index.html", text_after_transToEn=resultEn, text_after_transToRu=resultRu)
     if request.method == 'GET':
         return render_template('index.html')
 
